04eb565a879d36d6bd771e363689bd7f69ccd63a
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-05-21 15:13:56 +0700

    [seeds] chỉnh lại cách lặp

--- src/Commands/PostSeedCommand.php
+++ src/Commands/PostSeedCommand.php
@@ -61,47 +61,55 @@ class PostSeedCommand extends Command {
         $this->echoLog('Danh muc bai viet duoc tao thanh cong. So luong: '.count($post_categories));
 
         // Bài viết
-        $posts = [];
-        $seos = [];
-        $lang_metas = [];
-        for ($i=0; $i < 100; $i++) {
-            $name = 'Bài viết số '.$i;
-            $posts[] = [
-                'name' => $name,
-                'slug' => str_slug($name),
-                'detail' => $detail,
-                'status' => 1,
-                'created_at' => $created_at,
-                'updated_at' => $updated_at,
-            ];
-            $seos[] = [
-                'type'              => 'posts',
-                'type_id'           => $i+1,
-                'title'             => '',
-                'description'       => '',
-                'robots'            => 'Index,Follow',
-            ];
-            $lang_metas[] = [
-                'lang_table'        => 'posts',
-                'lang_table_id'     => $i+1,
-                'lang_locale'       => 'vi',
-                'lang_code'         => getCodeLangMeta()
-            ];
+        $stt = 0;
+        for ($j=0; $j < 10; $j++) { 
+            $posts = [];
+            $seos = [];
+            $lang_metas = [];
+            for ($i=0; $i < 1000; $i++) {
+                $stt++;
+                $name = 'Bài viết số '.$stt;
+                $posts[] = [
+                    'name' => $name,
+                    'slug' => str_slug($name),
+                    'detail' => $detail,
+                    'status' => 1,
+                    'created_at' => $created_at,
+                    'updated_at' => $updated_at,
+                ];
+                $seos[] = [
+                    'type'              => 'posts',
+                    'type_id'           => $stt+1,
+                    'title'             => '',
+                    'description'       => '',
+                    'robots'            => 'Index,Follow',
+                ];
+                $lang_metas[] = [
+                    'lang_table'        => 'posts',
+                    'lang_table_id'     => $stt+1,
+                    'lang_locale'       => 'vi',
+                    'lang_code'         => getCodeLangMeta()
+                ];
+            }
+            DB::table('posts')->insert($posts);
+            DB::table('seos')->insert($seos);
+            DB::table('language_metas')->insert($lang_metas);
         }
-        DB::table('posts')->insert($posts);
-        DB::table('seos')->insert($seos);
-        DB::table('language_metas')->insert($lang_metas);
-        $this->echoLog('Bai viet duoc tao thanh cong. So luong: '.count($posts));
+        $this->echoLog('Bai viet duoc tao thanh cong. So luong: '.$stt);
 
         // maps
-        $post_category_maps = [];
-        foreach (DB::table('posts')->get() as $post) {
-            $post_category_maps[] = [
-                'post_id' => $post->id,
-                'post_category_id' => rand(1,10),
-            ];
+        $stt = 0;
+        for ($j=0; $j < 10; $j++) { 
+            $post_category_maps = [];
+            for ($i=0; $i < 1000; $i++) {
+                $stt++;
+                $post_category_maps[] = [
+                    'post_id' => $stt,
+                    'post_category_id' => rand(1,10),
+                ];
+            }
+            \DB::table('post_category_maps')->insert($post_category_maps);
         }
-        DB::table('post_category_maps')->insert($post_category_maps);
     }
 
     public function echoLog($string) {
